1da012ab1ff4cd7726220c759f6c7ae2a03270e6
Aleksbgbg <kozichka01@abv.bg>
2018-09-13 18:17:48 +0100

    [Folder] Add model which represents a folder in the file system.

---
+++ RefMan/Models/Folder.cs
@@ -0,0 +1,19 @@
+﻿namespace RefMan.Models
+{
+    using System.Collections.Generic;
+
+    internal class Folder : FileSystemEntry
+    {
+        public Folder(string name) : base(name)
+        {
+            FileSystemEntries = new List<FileSystemEntry>();
+        }
+
+        public Folder(string name, IEnumerable<FileSystemEntry> fileSystemEntries) : base(name)
+        {
+            FileSystemEntries = new List<FileSystemEntry>(fileSystemEntries);
+        }
+
+        public List<FileSystemEntry> FileSystemEntries { get; }
+    }
+}
