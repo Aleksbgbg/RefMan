9d9918488d18af18d355a3291c2b49209c08aee4
Aleksbgbg <kozichka01@abv.bg>
2018-09-13 18:41:05 +0100

    [FolderViewModel] Add ViewModel which stores a Folder model.

---
+++ RefMan/ViewModels/FolderViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace RefMan.ViewModels
+{
+    using RefMan.Models;
+    using RefMan.ViewModels.Interfaces;
+
+    internal class FolderViewModel : ViewModelBase, IFolderViewModel
+    {
+        public Folder Folder { get; private set; }
+
+        public void Initialize(Folder folder)
+        {
+            Folder = folder;
+        }
+    }
+}
---
+++ RefMan/ViewModels/Interfaces/IFolderViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RefMan.ViewModels.Interfaces
+{
+    using RefMan.Models;
+
+    internal interface IFolderViewModel : IViewModelBase
+    {
+        void Initialize(Folder folder);
+    }
+}
