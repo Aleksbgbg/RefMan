8bcf0fbddeecb6f6b15476a3c41830cac2e02483
Aleksbgbg <kozichka01@abv.bg>
2018-09-13 19:26:07 +0100

    [AppBootstrapper] Register unregistered ViewModels.

--- RefMan/AppBootstrapper.cs
+++ RefMan/AppBootstrapper.cs
@@ -40,6 +40,10 @@
             // Register ViewModels
             _container.Singleton<IShellViewModel, ShellViewModel>();
             _container.Singleton<IMainViewModel, MainViewModel>();
+
+            _container.Singleton<IFileSystemViewModel, FileSystemViewModel>();
+
+            _container.PerRequest<IFolderViewModel, FolderViewModel>();
         }
 
         protected override object GetInstance(Type service, string key)
