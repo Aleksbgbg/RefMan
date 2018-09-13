ff2b7e8961956b6dda25f06d1c660f7a3d3be633
Aleksbgbg <kozichka01@abv.bg>
2018-09-13 19:24:27 +0100

    [FileSystem] Add ViewModels and Views for the file system display.

---
+++ RefMan/ViewModels/FileSystemViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace RefMan.ViewModels
+{
+    using RefMan.Factories.Interfaces;
+    using RefMan.Services.Interfaces;
+    using RefMan.ViewModels.Interfaces;
+
+    internal class FileSystemViewModel : ViewModelBase, IFileSystemViewModel
+    {
+        public FileSystemViewModel(IFileSystemFactory fileSystemFactory, IFileSystemService fileSystemService)
+        {
+            RootFolder = fileSystemFactory.MakeFolder(fileSystemService.ReadRootFolder());
+        }
+
+        public IFolderViewModel RootFolder { get; }
+    }
+}
---
+++ RefMan/ViewModels/Interfaces/IFileSystemViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace RefMan.ViewModels.Interfaces
+{
+    internal interface IFileSystemViewModel : IViewModelBase
+    {
+    }
+}
