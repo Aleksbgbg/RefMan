721316432c3b8d4662717ff419fe9f2d517e1b56
Aleksbgbg <kozichka01@abv.bg>
2018-09-14 15:01:00 +0100

    [FileSystemEntryViewModel] Add IsSelected property and an OnSelected method so that derived classes can handle selection.

--- RefMan/ViewModels/FileSystemEntryViewModel.cs
+++ RefMan/ViewModels/FileSystemEntryViewModel.cs
@@ -5,7 +5,7 @@
 
     internal abstract class FileSystemEntryViewModel<T> : ViewModelBase, IFileSystemEntryViewModel<T> where T : FileSystemEntry
     {
-        public T FileSystemEntry { get; protected set; }
+        public T FileSystemEntry { get; private protected set; }
 
         private bool _isExpanded;
         public virtual bool IsExpanded
@@ -19,6 +19,26 @@
                 _isExpanded = value;
                 NotifyOfPropertyChange(() => IsExpanded);
             }
+        }
+
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get => _isSelected;
+
+            set
+            {
+                if (_isSelected == value) return;
+
+                _isSelected = value;
+                NotifyOfPropertyChange(() => IsSelected);
+
+                OnSelected(_isSelected);
+            }
+        }
+
+        private protected virtual void OnSelected(bool isSelected)
+        {
         }
     }
 }
