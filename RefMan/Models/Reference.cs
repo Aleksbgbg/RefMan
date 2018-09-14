d2607e51c1ccb8f34da06f38e49e2f7009c07043
Aleksbgbg <kozichka01@abv.bg>
2018-09-14 15:07:04 +0100

    [Reference] Add model to store information about a hardvard referenced website.

---
+++ RefMan/Models/Reference.cs
@@ -0,0 +1,29 @@
+﻿namespace RefMan.Models
+{
+    using System;
+
+    internal class Reference
+    {
+        public Reference(string url, string pageTitle, string websiteName, string publisher, DateTime accessDate, int yearPublished)
+        {
+            Url = url;
+            PageTitle = pageTitle;
+            WebsiteName = websiteName;
+            Publisher = publisher;
+            AccessDate = accessDate;
+            YearPublished = yearPublished;
+        }
+
+        public string Url { get; }
+
+        public string PageTitle { get; }
+
+        public string WebsiteName { get; }
+
+        public string Publisher { get; }
+
+        public DateTime AccessDate { get; }
+
+        public int YearPublished { get; }
+    }
+}
