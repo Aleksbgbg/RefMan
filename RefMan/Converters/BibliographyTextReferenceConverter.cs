e50441a040994dc83d421dba81736083ce2a212a
Aleksbgbg <kozichka01@abv.bg>
2018-09-15 08:28:26 +0100

    [BibliographyTextReferenceConverter] Add converter to produce the bibliography citation string for a Reference object.

---
+++ RefMan/Converters/BibliographyTextReferenceConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace RefMan.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    using RefMan.Models;
+
+    [ValueConversion(typeof(Reference), typeof(string))]
+    internal class BibliographyTextReferenceConverter : IValueConverter
+    {
+        public static BibliographyTextReferenceConverter Default { get; } = new BibliographyTextReferenceConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Reference reference = (Reference)value;
+
+            return $"{reference.WebsiteName}. ({reference.YearPublished}) {reference.PageTitle}. [online] Available at: {reference.Url} [Accessed {reference.AccessDate:dd MMM. yy}]";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
