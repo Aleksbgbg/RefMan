56fe1349d560b53459a098e154f26d508b6b7afd
Aleksbgbg <kozichka01@abv.bg>
2018-09-15 08:25:08 +0100

    [InTextReferenceConverter] Add converter which produces in-text citations from a Reference.

---
+++ RefMan/Converters/InTextReferenceConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace RefMan.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    using RefMan.Models;
+
+    [ValueConversion(typeof(Reference), typeof(string))]
+    internal class InTextReferenceConverter : IValueConverter
+    {
+        public static InTextReferenceConverter Default { get; } = new InTextReferenceConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Reference reference = (Reference)value;
+
+            return $"({reference.WebsiteName}, {reference.YearPublished})";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
