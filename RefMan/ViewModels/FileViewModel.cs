9205d7318caa90ef361eb0ac4da6891d40ff0b73
Aleksbgbg <kozichka01@abv.bg>
2018-09-15 09:05:50 +0100

    [FileViewModel] When selected, publish FileSystemEntry to EventAggregator.

--- RefMan/ViewModels/FileViewModel.cs
+++ RefMan/ViewModels/FileViewModel.cs
@@ -1,10 +1,19 @@
 ﻿namespace RefMan.ViewModels
 {
+    using Caliburn.Micro;
+
     using RefMan.Models;
     using RefMan.ViewModels.Interfaces;
 
     internal class FileViewModel : FileSystemEntryViewModel<File>, IFileViewModel
     {
+        private readonly IEventAggregator _eventAggregator;
+
+        public FileViewModel(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+        }
+
         public override bool IsExpanded
         {
             get => base.IsExpanded;
@@ -17,6 +26,14 @@
         public void Initialize(File file)
         {
             FileSystemEntry = file;
+        }
+
+        private protected override void OnSelected(bool isSelected)
+        {
+            if (isSelected)
+            {
+                _eventAggregator.BeginPublishOnUIThread(FileSystemEntry);
+            }
         }
     }
 }
