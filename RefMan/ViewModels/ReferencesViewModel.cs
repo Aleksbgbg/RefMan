2c9098df65eb719024051d6b0b18dc6bca9f9c4f
Aleksbgbg <kozichka01@abv.bg>
2018-09-15 08:49:36 +0100

    [References] Store a collection of IReferenceViewModel objects and display their views.

--- RefMan/ViewModels/ReferencesViewModel.cs
+++ RefMan/ViewModels/ReferencesViewModel.cs
@@ -1,8 +1,11 @@
 ﻿namespace RefMan.ViewModels
 {
+    using Caliburn.Micro;
+
     using RefMan.ViewModels.Interfaces;
 
     internal class ReferencesViewModel : ViewModelBase, IReferencesViewModel
     {
+        public IObservableCollection<IReferenceViewModel> References { get; } = new BindableCollection<IReferenceViewModel>();
     }
 }
