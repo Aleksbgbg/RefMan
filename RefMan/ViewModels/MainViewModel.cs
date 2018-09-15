8b1699d8296f3d58a8e6aff0f21af12c719e9486
Aleksbgbg <kozichka01@abv.bg>
2018-09-15 18:01:28 +0100

    [Main] Hide ReferenceGeneratorViewModel until a file is loaded.

--- RefMan/ViewModels/MainViewModel.cs
+++ RefMan/ViewModels/MainViewModel.cs
@@ -1,17 +1,34 @@
 ﻿namespace RefMan.ViewModels
 {
+    using Caliburn.Micro;
+
+    using RefMan.Models;
     using RefMan.ViewModels.Interfaces;
 
-    internal class MainViewModel : ViewModelBase, IMainViewModel
+    internal class MainViewModel : ViewModelBase, IMainViewModel, IHandle<File>
     {
-        public MainViewModel(IFileSystemViewModel fileSystemViewModel, IReferenceGeneratorViewModel referenceGeneratorViewModel)
+        private bool _fileHasBeenSelected;
+
+        public MainViewModel(IEventAggregator eventAggregator, IFileSystemViewModel fileSystemViewModel, IReferenceGeneratorViewModel referenceGeneratorViewModel)
         {
             FileSystemViewModel = fileSystemViewModel;
-            ReferenceGeneratorViewModel = referenceGeneratorViewModel;
+            _referenceGeneratorViewModel = referenceGeneratorViewModel;
+
+            eventAggregator.Subscribe(this);
         }
 
         public IFileSystemViewModel FileSystemViewModel { get; }
 
-        public IReferenceGeneratorViewModel ReferenceGeneratorViewModel { get; }
+        private readonly IReferenceGeneratorViewModel _referenceGeneratorViewModel;
+        public IReferenceGeneratorViewModel ReferenceGeneratorViewModel => _fileHasBeenSelected ? _referenceGeneratorViewModel : null;
+
+        public void Handle(File message)
+        {
+            if (!_fileHasBeenSelected)
+            {
+                _fileHasBeenSelected = true;
+                NotifyOfPropertyChange(() => ReferenceGeneratorViewModel);
+            }
+        }
     }
 }
