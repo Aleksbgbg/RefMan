a2ff29da76e3f29f9f7ea457e9cbaefe6eed7fc2
Aleksbgbg <kozichka01@abv.bg>
2018-09-15 10:17:21 +0100

    [ReferenceGeneratorViewModel] Add 'Reference(url)' method which uses a referencing service to asynchronously download the webpage and produce a reference. This is added to the ReferencesViewModel.

--- RefMan/ViewModels/ReferenceGeneratorViewModel.cs
+++ RefMan/ViewModels/ReferenceGeneratorViewModel.cs
@@ -1,14 +1,32 @@
 ﻿namespace RefMan.ViewModels
 {
+    using System.Collections.Generic;
+
+    using Caliburn.Micro;
+
+    using RefMan.Models;
+    using RefMan.Services.Interfaces;
     using RefMan.ViewModels.Interfaces;
 
     internal class ReferenceGeneratorViewModel : ViewModelBase, IReferenceGeneratorViewModel
     {
-        public ReferenceGeneratorViewModel(IReferencesViewModel referencesViewModel)
+        private readonly IReferencingService _referencingService;
+
+        public ReferenceGeneratorViewModel(IReferencingService referencingService, IReferencesViewModel referencesViewModel)
         {
+            _referencingService = referencingService;
             ReferencesViewModel = referencesViewModel;
         }
 
         public IReferencesViewModel ReferencesViewModel { get; }
+
+        public IEnumerable<IResult> Reference(string url)
+        {
+            TaskResult<Reference> referenceTask = _referencingService.Reference(url).AsResult();
+
+            yield return referenceTask;
+
+            ReferencesViewModel.Add(referenceTask.Result);
+        }
     }
 }
