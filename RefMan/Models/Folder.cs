ad58b90b4d0167254b4b9b20bef74f6b7368a912
Aleksbgbg <kozichka01@abv.bg>
2018-09-16 14:35:15 +0100

    [Folder] Add IsExpanded property which dictates whether a Folder node is expanded by the user, with JSON decorations. Also add a JSON contructor to set this property.

--- RefMan/Models/Folder.cs
+++ RefMan/Models/Folder.cs
@@ -1,9 +1,20 @@
 ﻿namespace RefMan.Models
 {
+    using Newtonsoft.Json;
+
     internal class Folder : FileSystemEntry
     {
         public Folder(string path, string name) : base(path, name)
         {
         }
+
+        [JsonConstructor]
+        public Folder(string path, string name, bool isExpanded) : base(path, name)
+        {
+            IsExpanded = isExpanded;
+        }
+
+        [JsonProperty("is_expanded")]
+        public bool IsExpanded { get; set; }
     }
 }
