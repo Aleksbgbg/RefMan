2d943ead47e71d5b10f36451992c1c71160f92a1
Aleksbgbg <kozichka01@abv.bg>
2018-09-16 20:57:54 +0100

    [Reference] Add 'OpenInBrowser' button, which opens the reference URL in the browser.

--- RefMan/ViewModels/ReferenceViewModel.cs
+++ RefMan/ViewModels/ReferenceViewModel.cs
@@ -14,13 +14,16 @@
 
         private readonly IReferencingService _referencingService;
 
+        private readonly IWebService _webService;
+
         private File _referenceFile;
 
-        public ReferenceViewModel(IEventAggregator eventAggregator, IFileSystemService fileSystemService, IReferencingService referencingService)
+        public ReferenceViewModel(IEventAggregator eventAggregator, IFileSystemService fileSystemService, IReferencingService referencingService, IWebService webService)
         {
             _eventAggregator = eventAggregator;
             _fileSystemService = fileSystemService;
             _referencingService = referencingService;
+            _webService = webService;
         }
 
         public Reference Reference { get; private set; }
@@ -61,6 +64,11 @@
         public void Delete()
         {
             _eventAggregator.BeginPublishOnUIThread(this);
+        }
+
+        public void OpenInBrowser()
+        {
+            _webService.OpenInBrowser(Reference.Url);
         }
     }
 }
