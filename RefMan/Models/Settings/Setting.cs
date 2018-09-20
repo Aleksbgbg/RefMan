9168d7803d62f50436447f7763c1f30834103189
Aleksbgbg <kozichka01@abv.bg>
2018-09-20 15:16:54 +0100

    [Setting] Add setting model to store a name and description, including a value, for each setting.

---
+++ RefMan/Models/Settings/Setting.cs
@@ -0,0 +1,25 @@
+﻿namespace RefMan.Models.Settings
+{
+    using Newtonsoft.Json;
+
+    internal class Setting
+    {
+        [JsonConstructor]
+        public Setting(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public Setting(string name, string description, object value) : this(name, description)
+        {
+            Value = value;
+        }
+
+        public string Name { get; }
+
+        public string Description { get; }
+
+        public object Value { get; set; }
+    }
+}
