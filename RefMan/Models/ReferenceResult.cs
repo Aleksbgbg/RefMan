3c8052ee7f849b3a2bf169d24f4a49f245eef9a4
Aleksbgbg <kozichka01@abv.bg>
2018-11-08 19:32:46 +0000

    [ReferenceResult] Add result model which contains a reference, and whether or not it has been entirely filled out.

---
+++ RefMan/Models/ReferenceResult.cs
@@ -0,0 +1,29 @@
+﻿namespace RefMan.Models
+{
+    using Caliburn.Micro;
+
+    internal class ReferenceResult : PropertyChangedBase
+    {
+        public ReferenceResult(Reference reference, bool isComplete = true)
+        {
+            Reference = reference;
+            _isComplete = isComplete;
+        }
+
+        public Reference Reference { get; }
+
+        private bool _isComplete;
+        public bool IsComplete
+        {
+            get => _isComplete;
+
+            set
+            {
+                if (_isComplete == value) return;
+
+                _isComplete = value;
+                NotifyOfPropertyChange(() => IsComplete);
+            }
+        }
+    }
+}
