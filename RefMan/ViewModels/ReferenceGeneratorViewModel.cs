a4b7e91d34c121d29c429d80297c5e821113957d
Aleksbgbg <kozichka01@abv.bg>
2018-11-09 20:42:48 +0000

    [ReferenceGeneratorViewModel] Delegate referencing to ReferenceViewModel itself.

--- RefMan/ViewModels/ReferenceGeneratorViewModel.cs
+++ RefMan/ViewModels/ReferenceGeneratorViewModel.cs
@@ -5,16 +5,12 @@
     using Caliburn.Micro;
 
     using RefMan.Models;
-    using RefMan.Services.Interfaces;
     using RefMan.ViewModels.Interfaces;
 
     internal class ReferenceGeneratorViewModel : ViewModelBase, IReferenceGeneratorViewModel
     {
-        private readonly IReferencingService _referencingService;
-
-        public ReferenceGeneratorViewModel(IReferencingService referencingService, IReferencesViewModel referencesViewModel)
+        public ReferenceGeneratorViewModel(IReferencesViewModel referencesViewModel)
         {
-            _referencingService = referencingService;
             ReferencesViewModel = referencesViewModel;
         }
 
@@ -24,11 +20,7 @@
         {
             foreach (string url in urls.Split('\n'))
             {
-                TaskResult<Reference> referenceTask = _referencingService.Reference(url).AsResult();
-
-                yield return referenceTask;
-
-                ReferencesViewModel.Add(referenceTask.Result);
+                yield return ReferencesViewModel.Add(new ReferenceResult(new Reference(url))).AsResult();
             }
         }
     }
