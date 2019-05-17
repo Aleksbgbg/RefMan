6469e0cbdd3a11d741b59d2188cc0c39bf31a7f8
Aleksbgbg <kozichka01@abv.bg>
2019-05-17 20:55:48 +0100

    [Refman.Tests] Add unit test project.

---
+++ Refman.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Refman.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Refman.Tests")]
+[assembly: AssemblyCopyright("Copyright © Aleks Todorov 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3ec92145-c774-4a10-99c0-b97384d9f0ef")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
